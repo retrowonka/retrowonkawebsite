bdaf64d7704a3450b9c28ac20f38c597fe251a3e
retrowonka <72361947+retrowonka@users.noreply.github.com>
2025-10-08 12:14:44 -0500

    blog15addition

--- js/blog-loader.js
+++ js/blog-loader.js
@@ -4,6 +4,7 @@ const singlePost = urlParams.get('post');
 
 // Load just one if specified, otherwise all
 const postFiles = singlePost ? [singlePost] : [
+  'post15.md',
   'post14.md',
   'post13.md',
   'post12.md',
